981144497227ee2cb4be55ef7cee2162a43799e0
Alex <fantomer2007@mail.ru>
2021-02-15 16:14:02 +0300

    better news rendering

--- js/js1.js
+++ js/js1.js
@@ -1,7 +1,26 @@
 // *************СОЗДАЕМ НОВОСТИ*************
 const navNews = document.querySelector(".navNews");
 navNews.textContent = "Новости";
-
+const allNewsPosts = [
+  {
+    paragraph:
+      "С фотогалереей пока не срослось, но теперь состав команды формируется из актуального ростера на Альфа-лиге.",
+    author: "Condor",
+    date: "09.09.2020",
+  },
+  {
+    paragraph:
+      "Следующим этапом будет открытие фотогалереи и оптимизация кода. Ждем ваши отзывы и пожелания!",
+    author: "Condor",
+    date: "07.04.2019",
+  },
+  {
+    paragraph:
+      "Добро пожаловать на обновленный сайт команды Panic! Он почти адаптивный и в дальнейшем его ждет множество изменений, следите за новостями!",
+    author: "Condor",
+    date: "07.04.2019",
+  },
+];
 let addNews = function (e) {
   const content = document.querySelector(".mainclass");
   content.innerHTML = "";
@@ -10,43 +29,61 @@ let addNews = function (e) {
   document.querySelector(".side_right").classList.remove("hidden");
   //создаю внутренности для новости
 
-  var news = document.createElement("div");
-  news.classList.add("news");
-  var pNews = document.createElement("p");
-  pNews.innerHTML = `С фотогалереей пока не срослось, но теперь состав команды формируется из актуального ростера на Альфа-лиге.`;
-  var author = document.createElement("div");
-  author.classList.add("author");
-  author.innerHTML = "Condor<br> 09.09.2020";
-  news.append(pNews);
-  news.append(author);
-  //добавляю внутренности в новость
-  content.append(news);
+  function renderPosts() {
+    allNewsPosts.forEach((e) => {
+      let news = document.createElement("div");
+      let pNews = document.createElement("p");
+      let author = document.createElement("div");
+      news.classList.add("news");
+      pNews.innerHTML = `${e.paragraph}`;
+      author.classList.add("author");
+      author.innerHTML = `${e.author}<br> ${e.date}`;
+      news.append(pNews);
+      news.append(author);
+      //добавляю внутренности в новость
+      content.append(news);
+    });
+    navNews.removeEventListener("click", addNews);
+    navPlayers.addEventListener("click", addPlayers);
+  }
+  renderPosts();
+  // var news = document.createElement("div");
+  // news.classList.add("news");
+  // var pNews = document.createElement("p");
+  // pNews.innerHTML = `С фотогалереей пока не срослось, но теперь состав команды формируется из актуального ростера на Альфа-лиге.`;
+  // var author = document.createElement("div");
+  // author.classList.add("author");
+  // author.innerHTML = "Condor<br> 09.09.2020";
+  // news.append(pNews);
+  // news.append(author);
+  // //добавляю внутренности в новость
+  // content.append(news);
 
-  var news = document.createElement("div");
-  news.classList.add("news");
+  // var news = document.createElement("div");
+  // news.classList.add("news");
 
-  var pNews = document.createElement("p");
-  pNews.innerHTML = `Следующим этапом будет открытие фотогалереи и оптимизация кода. Ждем ваши отзывы и пожелания!`;
-  var author = document.createElement("div");
-  author.classList.add("author");
-  author.innerHTML = "Condor<br> 07.04.2019";
-  news.append(pNews);
-  news.append(author);
-  //добавляю внутренности в новость
-  content.append(news);
+  // var pNews = document.createElement("p");
+  // pNews.innerHTML = `Следующим этапом будет открытие фотогалереи и оптимизация кода. Ждем ваши отзывы и пожелания!`;
+  // var author = document.createElement("div");
+  // author.classList.add("author");
+  // author.innerHTML = "Condor<br> 07.04.2019";
+  // news.append(pNews);
+  // news.append(author);
+  // //добавляю внутренности в новость
+  // content.append(news);
 
-  var news = document.createElement("div");
-  news.classList.add("news");
-  var pNews = document.createElement("p");
-  pNews.textContent =
-    "Добро пожаловать на обновленный сайт команды Panic! Он почти адаптивный и в дальнейшем его ждет множество изменений, следите за новостями!";
-  var author = document.createElement("div");
-  author.classList.add("author");
-  author.innerHTML = "Condor<br> 07.04.2019";
-  news.append(pNews);
-  news.append(author);
-  //добавляю внутренности в новость
-  content.append(news);
+  // var news = document.createElement("div");
+  // news.classList.add("news");
+  // var pNews = document.createElement("p");
+  // pNews.textContent =
+  //   "Добро пожаловать на обновленный сайт команды Panic! Он почти адаптивный и в дальнейшем его ждет множество изменений, следите за новостями!";
+  // var author = document.createElement("div");
+  // author.classList.add("author");
+  // author.innerHTML = "Condor<br> 07.04.2019";
+  // news.append(pNews);
+  // news.append(author);
+  // //добавляю внутренности в новость
+  // content.append(news);
 
   //убираю событие клика по строке "Новости"
   navNews.removeEventListener("click", addNews);
@@ -76,18 +113,7 @@ let addPlayers = function () {
   fetch("https://alpha.tl/api?clan=1")
     .then((data) => data.json())
     .then((json) => {
-      const responseGood = json.players;
-      responseGood.forEach((post) => {
-        const Player = document.createElement("div");
-        Player.classList.add("player", post.race);
-        const PlayerName = document.createElement("div");
-        PlayerName.innerHTML = `<a href="${post.bnetaccount}" target='_blank'>${post.nickname}</a>`;
-        const Playerdesc = document.createElement("p");
-        Playerdesc.textContent = post.league;
-        Player.append(PlayerName);
-        Player.append(Playerdesc);
-        content.append(Player);
-      });
+      renderPlayers(json);
     })
     .catch(() => {
       console.error("ERROR!");
@@ -96,6 +122,21 @@ let addPlayers = function () {
     .finally(() => {
       statusMessage.remove();
     });
+
+  function renderPlayers(json) {
+    const responseGood = json.players;
+    responseGood.forEach((post) => {
+      const Player = document.createElement("div");
+      Player.classList.add("player", post.race);
+      const PlayerName = document.createElement("div");
+      PlayerName.innerHTML = `<a href="${post.bnetaccount}" target='_blank'>${post.nickname}</a>`;
+      const Playerdesc = document.createElement("p");
+      Playerdesc.textContent = post.league;
+      Player.append(PlayerName);
+      Player.append(Playerdesc);
+      content.append(Player);
+    });
+  }
 
   content.classList.add("players");
   content.classList.remove("content");
